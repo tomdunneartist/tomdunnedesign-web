cf627fb254f886321a2d642fb11bc7513e7fa60e
Tom Dunne <tomdunneartist@gmail.com>
2023-05-09 21:20:32 -0400

    Portfolio update 2023

--- js/main.js
+++
@@ -1,9 +0,0 @@
-$('.here').click(function () {
-    console.log('you clicked');
-    window.location.assign('selected.html')
-})
-
-$('.here2').click(function () {
-    console.log('you click');
-    window.location.assign('index.html')
-})
